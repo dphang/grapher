d98832394f8171231508ec88e56579e1389879e4
dphang <dphang12@gmail.com>
2013-07-01 00:29:52 -0700

    Deleted extraneous files

--- grapher.js
+++ grapher.js
@@ -774,3 +774,189 @@ function makeSubscripts(text)
     return text;
 }
 
+// Undirected edge between two vertices v1 and v2
+function Edge(v1, v2)
+{
+    this.v1 = v1;
+    this.v2 = v2;
+    this.text = '';
+}
+
+// Draw the edge: only a line since it's undirected
+Edge.prototype.draw = function(c) 
+{
+    // Nearest points on v1, v2 circles
+    var point1 = this.v1.nearestCirclePoint(this.v2.x, this.v2.y);
+    var point2 = this.v2.nearestCirclePoint(this.v1.x, this.v1.y);
+    
+    c.beginPath();
+    c.moveTo(point1.x, point1.y);
+    c.lineTo(point2.x, point2.y);
+    c.stroke();
+    
+    var midX = (point2.x + point1.x) / 2;
+    var midY = (point2.y + point1.y) / 2;   
+    var angle = Math.atan2(point2.x - point1.x, point2.y - point1.y);
+    
+    // Draw text
+    drawText(c, this.text, midX, midY, angle, false);
+};
+
+// Check if edge contains a point (x, y)
+Edge.prototype.contains = function(x, y)
+{
+    var dx = this.v2.x - this.v1.x;
+    var dy = this.v2.y - this.v1.y;
+    var length = Math.sqrt(dx * dx + dy * dy);
+    var percent = (dx * (x - this.v1.x) + dy * (y - this.v1.y)) / (length * length);
+    var dist = (dx * (y - this.v1.y) - dy * (x - this.v1.x)) / length;
+    return (percent > 0 && percent < 1 && Math.abs(dist) < EDGE_MARGIN);
+};
+
+// Temporary edge from a source vertex to a point
+function TempEdge(vertex, dest)
+{
+    this.vertex = vertex;
+    this.dest = dest;
+}
+
+// Drawing function for the edge
+TempEdge.prototype.draw = function(c)
+{
+    var point = this.vertex.nearestCirclePoint(this.dest.x, this.dest.y);
+
+    c.beginPath();
+    c.moveTo(point.x, point.y);
+    c.lineTo(this.dest.x, this.dest.y);
+    c.stroke(); 
+}
+
+
+// Directed edge
+function DirEdge(v1, v2)
+{
+    this.v1 = v1;
+    this.v2 = v2;
+    this.text = '';
+}
+
+// Draw a directed edge, with an arrow
+DirEdge.prototype.draw = function(c) 
+{
+    c.beginPath();
+    
+    // Nearest points on vertex circles
+    var point1 = this.v1.nearestCirclePoint(this.v2.x, this.v2.y);
+    var point2 = this.v2.nearestCirclePoint(this.v1.x, this.v1.y);
+    c.moveTo(point1.x, point1.y);
+    c.lineTo(point2.x, point2.y);
+    c.stroke();
+    
+    // Draw the arrow
+    drawArrow(c, point2.x, point2.y, Math.atan2(point2.y - point1.y, point2.x - point1.x));
+    
+    var midX = (point2.x + point1.x) / 2;
+    var midY = (point2.y + point1.y) / 2;   
+    var angle = Math.atan2(point2.x - point1.x, point2.y - point1.y);
+    
+    // Draw text
+    drawText(c, this.text, midX, midY, angle, false);
+};
+
+// Check whether the edge contains a point (x, y) (for selecting the edge)
+DirEdge.prototype.contains = function(x, y)
+{
+    var dx = this.v2.x - this.v1.x;
+    var dy = this.v2.y - this.v1.y;
+    var length = Math.sqrt(dx * dx + dy * dy);
+    var percent = (dx * (x - this.v1.x) + dy * (y - this.v1.y)) / (length * length);
+    var dist = (dx * (y - this.v1.y) - dy * (x - this.v1.x)) / length;
+    return (percent > 0 && percent < 1 && Math.abs(dist) < EDGE_MARGIN);
+};
+
+// Temporary directed edge from a source vertex to a point
+function TempDirEdge(vertex, dest)
+{
+    this.vertex = vertex;
+    this.dest = dest;
+}
+
+// Drawing function
+TempDirEdge.prototype.draw = function(c)
+{
+    var point = this.vertex.nearestCirclePoint(this.dest.x, this.dest.y);
+
+    c.beginPath();
+    c.moveTo(point.x, point.y);
+    c.lineTo(this.dest.x, this.dest.y);
+    c.stroke();
+    
+    drawArrow(c, this.dest.x, this.dest.y, Math.atan2(this.dest.y - point.y, this.dest.x - point.x));
+}
+
+function Vertex(x, y)
+{
+    this.x = x;
+    this.y = y;
+    this.radius = VERTEX_RADIUS;
+    this.mouseOffsetX = 0;
+    this.mouseOffsetY = 0;
+    this.text = '';
+}
+
+Vertex.prototype.draw = function(c) 
+{
+    // Change vertex radius if text width is too large or small
+    var textWithoutSubscripts = makeSubscripts(this.text);
+    var width = c.measureText(textWithoutSubscripts).width;
+    if (width < 2 * VERTEX_RADIUS - VERTEX_RADIUS / 3)
+    {
+        this.radius = VERTEX_RADIUS;
+    }
+    else
+    {
+        this.radius = width / 2 + VERTEX_PADDING;
+    }
+
+    // Draw the vertex circle
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    c.stroke();
+    
+    // Draw the text
+    drawText(c, this.text, this.x, this.y, null, selectedObject == this);
+};
+
+// Check if a point is inside the vertex
+Vertex.prototype.contains = function(x, y) 
+{
+    var dx = x - this.x;
+    var dy = y - this.y;
+    return dx * dx + dy * dy <= this.radius * this.radius;
+};
+
+// Find the nearest point on the edge of the circle to the given coordinates
+Vertex.prototype.nearestCirclePoint = function(x, y)
+{
+    var dx = x - this.x;
+    var dy = y - this.y;
+    var dist = Math.sqrt(dx * dx + dy * dy);
+    return {
+        'x': this.x + this.radius * dx / dist,
+        'y': this.y + this.radius * dy / dist
+    };
+};
+
+// Keep track of start point (for moving vertices), so that later, we can move vertex without having to center it on the mouse
+Vertex.prototype.setStartPoint = function(x, y)
+{
+    this.mouseOffsetX = this.x - x;
+    this.mouseOffsetY = this.y - y;
+};
+
+// Set the position of the vertex, using the offset point set earlier
+Vertex.prototype.setAnchorPoint = function(x, y)
+{
+    this.x = x + this.mouseOffsetX;
+    this.y = y + this.mouseOffsetY;
+};
